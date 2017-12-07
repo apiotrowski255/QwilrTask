241053806eabf7df2e8bd8a0d2cf358ee6c01f0e
awesomea900 <awesomea900@gmail.com>
2017-12-07 16:36:00 +1100

    user details and API sorta working, used dummy buttons

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from data import data
+import requests
+import json
 
 app = Flask(__name__)
 
+data = data()
+
 @app.route('/')
+@app.route('/home')
 def homepage():
-   return render_template("main.html")
+   return render_template("home.html", data=data)
+   
+@app.route('/test')
+def testAPI():
+
+   try:
+      r = requests.get('https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_INTRADAY&symbol=BTC&market=AUD&apikey=QY1GOJ78M3026VJH')
+   except requests.ConnectionError:
+      return "Connection Error"
+   
+   jsonData = r.text
+   jsonToPython = json.loads(jsonData)
+   return render_template("displayInfo.html", data=jsonToPython)
+
    
    
 if __name__ == "__main__":
---
+++ data.py
@@ -0,0 +1,11 @@
+def data():
+   profile = {
+         'name': 'Jonah',
+         'account_funds': 555,
+         'stocks': {
+            'BTC': 1,
+            'YHOO': 2
+         }
+      }
+   
+   return profile
